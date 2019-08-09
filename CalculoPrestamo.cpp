73f5460864c6330cae71e5570fea9d90cc04efc2
ESCINF <ESCINF@LEN-M83>
2019-08-09 09:20:50 -0600

    asd

--- CalculoPrestamo.cpp
+++ CalculoPrestamo.cpp
@@ -4,11 +4,12 @@
 
 #include <iostream>
 #include "CalculoPrestamo.h"
-
+//USAR FIND Y SUBSTRING (BUSCAR MAS EN INTERNET
+//8.50% substring(0,4) = "8.50" std::stof("8.50") = 8.50 METODO OBTENER PORCENTAJE
 float calculoPrestamo::obtenerPorcentaje(std::string porcentajeTXT) {
-    return 0.04;
+    
 }
-
+//PARECIDO AL ANTERIOR 2A.find("A") = 1 , 2A.find("M") = 0 castear de string a int std::stoi("2")= 2
 int calculoPrestamo::calcularTiempoEnMeses() {
 
 }
@@ -24,7 +25,7 @@ calculoPrestamo::CalculoPrestamo() {
 calculoPrestamo::CalculoPrestamo(int64_t tiempo) {
 
 }
-
+//std::string reporte = "texto \n"
 string calculoPrestamo::reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT) {
 
 }
