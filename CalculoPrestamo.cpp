7ceb0567555bd33cea5fab92740e32f0326086cd
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-12 13:32:04 -0600

    agreg metodo obtener porcentaje

--- CalculoPrestamo.cpp
+++ CalculoPrestamo.cpp
@@ -5,14 +5,21 @@
 #include <iostream>
 #include "CalculoPrestamo.h"
 //USAR FIND Y SUBSTRING (BUSCAR MAS EN INTERNET
-//8.50% substring(0,4) = "8.50" std::stof("8.50") = 8.50 METODO OBTENER PORCENTAJE
+//8.50% substring(0,4) = "8.40" std::stof("8.50") = 8.50 METODO OBTENER PORCENTAJE
 float calculoPrestamo::obtenerPorcentaje(string porcentajeTXT) {
-    if(porcentajeTXT.find(A)== true)
-        return getMonto();
+    float x;
+    x = porcentajeTXT.substring(0,4);
+    x = std::stof(x);
+    x = x * 100;
+    return x;
 }
 //PARECIDO AL ANTERIOR 2A.find("A") = 1 , 2A.find("M") = 0 castear de string a int std::stoi("2")= 2
 int calculoPrestamo::calcularTiempoEnMeses() {
-
+    if(porcentajeTXT.find(A)== true)
+        return getMonto() * 12;
+    else{
+        return get
+    }
 }
 
 float calculoPrestamo::calcularInteresMensual() {
@@ -28,8 +35,9 @@ calculoPrestamo::CalculoPrestamo(int monto) {
 }
 //std::string reporte = "texto \n"
 string calculoPrestamo::reporteCalculoPrestamo(string tiempoTXT, string porcentajeTXT) {
+    string reporte = ".";
     for(int i=0;i<12;i++){
-        string reporte=
+        reporte= "x";
     }
 
 }
