0b67f8716ff4eed6609f9f7de6708a4fafdaa308
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-12 18:14:33 -0600

    terminar reporte

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
-#include "CalculoPrestamo.h"
+//
+// Created by Alonso Chaves  on 2019-08-12.
+//
+#include "CalculoPrestamo.cpp"
 
-int main() {
-    return 0;
+int main (int argc, char *argv[]) {
+	string tiempo = "1A";
+	string tasaAnual = "2.00%";
+	string porcentaje = "8.40%";
+	calculoPrestamo prestamo1;
+	
+	cout<<prestamo1.reporteCalculoPrestamo(tiempo, porcentaje)<<endl;
+	
+	return 0;
 }
+
