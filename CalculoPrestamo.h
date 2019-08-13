41ff8c4d86acb1bddc1e2fdf42cf55c27e770a15
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-12 18:15:27 -0600

    terminar reporte

--- CalculoPrestamo.h
+++ CalculoPrestamo.h
@@ -1,27 +1,27 @@
 //
-// Created by Maikol Guzman  on 2019-08-08.
+// Created by Alonso Chaves  on 2019-08-12.
 //
-
 #ifndef CALCULOPRESTAMO_CALCULOPRESTAMO_H
 #define CALCULOPRESTAMO_CALCULOPRESTAMO_H
-
-
 #include <cstdint>
 #include <string>
+#include <iostream>
 using std::string;
+using std::cout;
+using std::endl;
 
 class calculoPrestamo{
 private:
-    float monto;
-    float obtenerPorcentaje();
-    int calcularTiempoEnMeses();
-    float calcularInteresMensual();
+	float monto;
+	float obtenerPorcentaje(std::string porcentajeTXT);
+	int calcularTiempoEnMeses(std::string);
+	float calcularInteresMensual(float balance, float tasaAnual);
 public:
-    CalculoPrestamo();
-    CalculoPrestamo(string);
-    string reporteCalculoPrestamo();
-    float getMonto();
-    void setMonto();
+	calculoPrestamo();
+	calculoPrestamo(float);
+	string reporteCalculoPrestamo(string tiempoTXT, string porcentajeTXT);
+	float getMonto();
+	void setMonto(float m);
 };
 
 #endif //CALCULOPRESTAMO_CALCULOPRESTAMO_H
