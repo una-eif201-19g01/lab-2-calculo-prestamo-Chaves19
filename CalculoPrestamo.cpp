8d5d08b5e0e05a12763c1645d443d4cebd956159
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-14 14:23:41 -0600

    casi terminado

--- CalculoPrestamo.cpp
+++ CalculoPrestamo.cpp
@@ -3,61 +3,62 @@
 //
 #include "CalculoPrestamo.h"
 
-float CalculoPrestamo::obtenerPorcentaje(std::string porcentajeTXT) {
-	int porc = 0;
-	porc = porcentajeTXT.find("%");
-	porcentajeTXT = porcentajeTXT.substr(0,porc);
-	float tasaAnual = std::stof(porcentajeTXT);
-	return tasaAnual * 100;
+float CalculoPrestamo::obtenerPorcentaje(string porcentajeTXT) {
+    int porc = 0;
+    porc = porcentajeTXT.find("%");
+    porcentajeTXT = porcentajeTXT.substr(0, porc);
+    float tasaAnual = std::stof(porcentajeTXT);
+    return tasaAnual * 100;
 }
 
 int CalculoPrestamo::calcularTiempoEnMeses(string tiempoTXT) {
     int x = 0;
     int meses = 0;
-    if(tiempoTXT.find("A") == true){
-	x = tiempoTXT.find("A");
+    if (tiempoTXT.find("A") == true) {
+        x = tiempoTXT.find("A");
         tiempoTXT.substr(0, x);
-	meses = std::stoi(tiempoTXT);
-	meses = meses * 12;
-    }else{
-        x = (tiempoTXT.find("M");
-        tiempoTXT = tiempoTXT.substr(0,x);
-        meses = std::stoi(tiempoTXT);	
+        meses = std::stoi(tiempoTXT);
+        meses = meses * 12;
+    } else {
+        x = tiempoTXT.find("M");
+        tiempoTXT = tiempoTXT.substr(0, x);
+        meses = std::stoi(tiempoTXT);
     }
     return meses;
 }
 
 float CalculoPrestamo::calcularInteresMensual(float balance, float tasaAnual) {
-	return (balance * (tasaAnual * 12));	
+    return (balance * (tasaAnual * 12));
 }
 
 CalculoPrestamo::calculoPrestamo() {
-	monto = 100000;
+    monto = 100000;
 }
 
-CalculoPrestamo::calculoPrestamo(float m){
-	monto = m;
+CalculoPrestamo::calculoPrestamo(float m) {
+    monto = m;
 }
-//std::string reporte = "texto \n"
-std::string CalculoPrestamo::reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT) {
-    std::string reporte = ".";
+
+
+string CalculoPrestamo::reporteCalculoPrestamo(string tiempoTXT, string porcentajeTXT) {
+    string reporte = ".";
     float balance = getMonto();
     float balanceNuevo = 0;
     float y = obtenerPorcentaje(porcentajeTXT);
     float z = calcularTiempoEnMeses(tiempoTXT);
-    for(int i=0;i<z;i++){
-	balance = balanceNuevo;
+    for (int i = 0; i < z; i++) {
+        balance = balanceNuevo;
         float interes = calcularInteresMensual(balance, y);
         balanceNuevo = balance + interes;
-        reporte = reporte + "Tasa [ "+ porcentajeTXT +" ], Mes [ "+ std::to_string(i + 1) +" ], balance inicial [ "+ std::to_string(balance) +" ], interes [ "+ std::to_string(interes) +" ], balance nuevo [ " + std::to_string(balanceNuevo) +" ] \n";
+        reporte = reporte + "Tasa [ " + porcentajeTXT + " ], Mes [ " + std::to_string(i + 1) + " ], balance inicial [ " + std::to_string(balance) + " ], interes [ " + std::to_string(interes) + " ], balance nuevo [ " + std::to_string(balanceNuevo) + " ] \n";
     }
     return reporte;
 }
 
 float CalculoPrestamo::getMonto() {
-	return monto;
+    return monto;
 }
 
 void CalculoPrestamo::setMonto(float m) {
-	monto = m;
+    monto = m;
 }
