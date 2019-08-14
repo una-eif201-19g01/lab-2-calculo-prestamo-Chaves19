75c584bbb91720865ecc1ddc82c7a39500160a51
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-14 14:23:49 -0600

    casi terminado

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 //
 #include "CalculoPrestamo.cpp"
 
-int main (int argc, char *argv[]) {
-	string tiempo = "1A";
-	string porcentaje = "8.40%";
-	calculoPrestamo prestamo1(30000);
-	
-	cout<<prestamo1.reporteCalculoPrestamo(tiempo, porcentaje)<<endl;
-	
-	return 0;
+int main(int argc, char *argv[]) {
+    string tiempo = "1A";
+    string porcentaje = "8.40%";
+    CalculoPrestamo prestamo1(30000);
+
+    cout << prestamo1.reporteCalculoPrestamo(tiempo, porcentaje) << endl;
+
+    return 0;
 }
 
