69d17b7cc5c70b2fc656d5bff3d23eaa6070fc43
Chaves19 <53483583+Chaves19@users.noreply.github.com>
2019-08-14 14:10:11 -0600

    casi terminado

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 //
-// Created by Alonso Chaves  on 2019-08-12.
+// Created by Alonso Chaves  on 2019-08-14.
 //
 #include "CalculoPrestamo.cpp"
 
 int main (int argc, char *argv[]) {
 	string tiempo = "1A";
-	string tasaAnual = "2.00%";
 	string porcentaje = "8.40%";
-	calculoPrestamo prestamo1;
+	calculoPrestamo prestamo1(30000);
 	
 	cout<<prestamo1.reporteCalculoPrestamo(tiempo, porcentaje)<<endl;
 	
